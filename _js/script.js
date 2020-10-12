49dbfeb1a912bdf784d18f645afbfc0214a479e3
MacksonWelton <mackson.welton@gmail.com>
2020-10-12 20:36:31 -0300

    Novas funcionalidades

--- _js/script.js
+++ _js/script.js
@@ -1,18 +1,30 @@
-const sideBarItems = document.querySelectorAll('.sidebar-item');
+const sideBarItems = document.querySelectorAll(".sidebar-item");
 
 function selectItemClick() {
-    removeSelected();
+  removeSelected();
 
-    this.classList.add('selected');
+  this.classList.add("selected");
 }
 
 function removeSelected() {
-    sideBarItems.forEach(item => {
-        item.classList.remove('selected');
-    })
+  sideBarItems.forEach((item) => {
+    item.classList.remove("selected");
+  });
 }
 
+sideBarItems.forEach((item) => {
+  item.addEventListener("click", selectItemClick);
+});
 
-sideBarItems.forEach(item => {
-    item.addEventListener('click', selectItemClick);
-})
+const callToAction = document.querySelectorAll(".call-to-action");
+
+function redirectLink(event) {
+    window.open(
+        'https://www.figma.com/proto/hxAkCN9CS5BwEHR8oBh3b6/Projeto-EasyPark-%2F-Wbrain?node-id=50%3A0&scaling=min-zoom',
+        '_blank'
+      );
+}
+
+callToAction.forEach((item) => {
+  item.addEventListener("click", redirectLink);
+});
