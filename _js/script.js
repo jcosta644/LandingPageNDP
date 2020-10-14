5774cc9a4419cac4377d951d67fbaf9e11a139b1
Jardel Costa <jfcosta@ic.ufal.br>
2020-10-14 14:41:32 -0300

    Adicionando o change active on scroll

--- _js/script.js
+++ _js/script.js
@@ -1,4 +1,11 @@
 const sideBarItems = document.querySelectorAll(".sidebar-item");
+const mainSections = document.querySelectorAll(".content");
+const callToAction = document.querySelectorAll(".call-to-action");
+
+const main = document.querySelector('#main');
+
+let lastId;
+let cur = [];
 
 function selectItemClick() {
   removeSelected();
@@ -12,19 +19,36 @@ function removeSelected() {
   });
 }
 
+function redirectLink(event) {
+  window.open(
+      'https://www.figma.com/proto/hxAkCN9CS5BwEHR8oBh3b6/Projeto-EasyPark-%2F-Wbrain?node-id=50%3A0&scaling=min-zoom',
+      '_blank'
+    );
+}
+
+function scrollEvent(event) {
+  var fromTop = $('#main').scrollTop();
+
+  sideBarItems.forEach(link => {
+    let section = document.querySelector(link.hash);
+
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop+1
+    ) {
+      link.classList.add("selected");
+    } else {
+      link.classList.remove("selected");
+    }
+  });
+}
+
 sideBarItems.forEach((item) => {
   item.addEventListener("click", selectItemClick);
 });
 
-const callToAction = document.querySelectorAll(".call-to-action");
-
-function redirectLink(event) {
-    window.open(
-        'https://www.figma.com/proto/hxAkCN9CS5BwEHR8oBh3b6/Projeto-EasyPark-%2F-Wbrain?node-id=50%3A0&scaling=min-zoom',
-        '_blank'
-      );
-}
-
 callToAction.forEach((item) => {
   item.addEventListener("click", redirectLink);
 });
+
+main.addEventListener("scroll", scrollEvent);
